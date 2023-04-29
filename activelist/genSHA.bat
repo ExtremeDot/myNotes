60d2fe4ba58fa3a636cb1a916b51c708c3daa88e
LostInPersia <120102306+ExtremeDot@users.noreply.github.com>
2023-04-29 15:47:32 +0330

    Update genSHA.bat

--- activelist/genSHA.bat
+++ activelist/genSHA.bat
@@ -3,23 +3,64 @@
 :menu
 cls
 echo Please select an option:
-echo 1. Generate SHA256 hash for Local Files
-echo 2. Generate SHA256 hash for ovpn.conf
-echo 3. Generate SHA256 hash for riglist.json
-echo 4. Generate SHA256 hash for network package
+echo Generate SHA256 hash for:
+echo 1. Local File
+echo 2. Online File
+echo 3. Online - ovpn.conf
+echo 4. Online - riglist.json
+echo 5. Online - network package
 echo 0. Exit
 
 set /p choice=Enter your choice:
 
 if "%choice%"=="1" goto sha256lcoal
-if "%choice%"=="2" goto sha256ovpn
-if "%choice%"=="3" goto sha256riglist
-if "%choice%"=="4" goto sha256networkpack
+if "%choice%"=="2" goto sha256online
+if "%choice%"=="3" goto sha256ovpn
+if "%choice%"=="4" goto sha256riglist
+if "%choice%"=="5" goto sha256networkpack
 if "%choice%"=="0" goto end
 
 echo Invalid choice. Please try again.
 pause
 goto menu
+
+
+:sha256online
+echo Enter the URL of the file you want to download and hash:
+set /p url=
+echo Enter the name of the file you want to save:
+set /p filename=
+
+echo Downloading file...
+curl -o %filename% %url%
+echo Generating SHA256 hash code for %filename%...
+certutil -hashfile "%filename%" SHA256 | findstr /r "[0-9A-Fa-f]*$" > %filename%_hash.txt
+echo Hash code generated successfully.
+
+if not exist "%filename%_hash.txt" (
+  echo Error: %filename%_hash.txt not found.
+  pause
+  goto end
+)
+
+setlocal enabledelayedexpansion
+
+set count=0
+color 0A
+for /f "tokens=*" %%a in (%filename%_hash.txt) do (
+  set /a count+=1
+  if !count! == 2 (
+    echo == Online Package HASH =========================================
+    echo %%a
+	echo ================================================================
+	set "secondline=%%a"
+    goto endloop
+  )
+)
+
+pause
+goto menu
+
 
 :sha256networkpack
 set "latest_online_netpack=https://raw.githubusercontent.com/ExtremeDot/myNotes/main/activelist/network-config.deb"
